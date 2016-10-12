7677302edf05fe8b8aff00b68eccef1288be2db2
ahmadarab <ahmad.arab@omnix.ae>
2016-10-12 17:02:54 +0300

    email first

--- resources/views/emails/valuecar.blade.php
+++ resources/views/emails/valuecar.blade.php
@@ -1,16 +1,6 @@
-<h1>value car</h1>
+
 @if(isset($brand_id)) brand:{{ $brand_id }} <br>@endif
 @if(isset($model_id))model:{{ $model_id }} <br>@endif
 @if(isset($first_name))first name:{{ $first_name }} <br>@endif
 @if(isset($last_name))last name:{{ $last_name }} <br>@endif
 @if(isset($phone_number))phone number:{{ $phone_number }} <br>@endif
-@if(isset($email))email:{{ $email }} <br>@endif
-@if(isset($best_time_to_contact))best time to contact:{{ $best_time_to_contact }} <br>@endif
-@if(isset($ownership_year))ownership year:{{ $ownership_year }} <br>@endif
-@if(isset($year))year:{{ $year}} <br>@endif
-@if(isset($body_condition))body condition:{{ $body_condition }} <br>@endif
-@if(isset($engine_condition))engine condition:{{ $engine_condition }} <br>@endif
-@if(isset($ac_condition))ac condition:{{ $ac_condition }} <br>@endif
-@if(isset($mileage))mileage:{{ $mileage }} <br>@endif
-@if(isset($gear_condition))gear condition:{{ $gear_condition }} <br>@endif
-@if(isset($message))message:{{ $message }} <br>@endif
